a4268f91643db1f2179d375d6f5149ecf63024dd
saiken27 <fominalexander29@gmail.com>
2025-10-30 11:30:38 +0300

    новый дизайн красивой киберпанк кнопки

--- src/components/UiButton/UiButton.vue
+++ src/components/UiButton/UiButton.vue
@@ -19,23 +19,23 @@ const props = withDefaults(defineProps<IProps>(), {
 
 <style module lang="scss">
 .button {
-  position: relative;
   display: inline-flex;
   gap: 8px;
   align-items: center;
   justify-content: center;
   height: 40px;
-  padding: 16px 32px;
+  padding: 0 24px;
+  font-family: monospace;
   font-size: 1rem;
-  line-height: 1.5;
   color: var(--color-white);
   background: var(--color-primary);
-  border-radius: 16px;
+  border: none;
+  border-radius: 6px;
 
   &[data-layout='secondary'] {
     color: var(--color-primary);
-    background-color: var(--color-transparent);
-    border-color: var(--color-primary);
+    background: transparent;
+    border: 1px solid var(--color-primary);
   }
 }
 </style>
